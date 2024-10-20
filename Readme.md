f2966334b1c762072eefd73a1fd8d259febd7e8a
Jhonatan Morais <jhonatanvinicius@gmail.com>
2024-10-20 14:30:22 -0300

    feat Add dapp to add and update reviews

---
+++ Readme.md
@@ -0,0 +1,22 @@
+
+
+Connect to devnet:
+ `solana config set -ud`
+
+Connect to localhost:
+`solana config set -ul`
+
+Request sols...
+`solana airdrop 2`
+
+Check which network is connected
+`solana config get`
+
+Build to solana:
+`cargo build-sbf`
+
+Deploy to solana
+`solana program deploy target/deploy/restaurant_review.so`
+
+1- Program Id: 6QtSSKrtk71FGYHE6MjNwHcRr6fc7yHJWRcvcV5Z7J9S
+2- https://explorer.solana.com/address/6QtSSKrtk71FGYHE6MjNwHcRr6fc7yHJWRcvcV5Z7J9S?cluster=devnet
---
+++ src/instructions.rs
@@ -0,0 +1,45 @@
+use borsh::BorshDeserialize;
+use borsh_derive::BorshSerialize;
+use solana_program::program_error::ProgramError;
+
+pub enum ReviewInstruction {
+    AddReview {
+        title: String,
+        rating: u8,
+        description: String,
+    },
+    UpdateReview {
+        title: String,
+        rating: u8,
+        description: String,
+    },
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+struct ReviewPayload {
+    title: String,
+    rating: u8,
+    description: String,
+}
+
+impl ReviewInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+        let payload = ReviewPayload::try_from_slice(rest).unwrap();
+        Ok(match variant {
+            0 => Self::AddReview {
+                title: payload.title,
+                rating: payload.rating,
+                description: payload.description,
+            },
+            1 => Self::UpdateReview {
+                title: payload.title,
+                rating: payload.rating,
+                description: payload.description,
+            },
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
